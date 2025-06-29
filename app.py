d9fb75550179441e86bdbd21ecec62a1ef4a27ac
Bao Bao C <baobaoc@MacBook-Pro.local>
2025-06-29 17:09:58 +0800

    fix: clean up duplicated code in app.py

--- app.py
+++ app.py
@@ -58,32 +58,7 @@ def handle_postback(event):
     if data.startswith("answer_"):
         answer_number = int(data.split("_")[1])
         handle_answer(user_id, answer_number)
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import os
-
-app = Flask(__name__)
-
-line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
     port = int(os.getenv("PORT", 5000))
     app.run(host='0.0.0.0', port=port) 
